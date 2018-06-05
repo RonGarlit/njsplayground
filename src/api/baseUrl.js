e7da349ab59bd52888e8f0b1f33f68c8b5a68c11
RonGarlit <rongarlit@live.com>
2018-06-05 13:33:39 -0400

    Updated Babel, ESLint, added NPM Lint scripts, fixed baseurl.js

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -4,7 +4,9 @@ export default function getBaseUrl() {
 
 function getQueryStringParameterByName(name, url) {
   if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
+  // lint change: name = name.replace(/[\[\]]/g, "\\$&");
+  // to:
+  name = name.replace(/[[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
       results = regex.exec(url);
   if (!results) return null;
